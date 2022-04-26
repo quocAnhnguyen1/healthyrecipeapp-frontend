cc94e456cdbf629c720cee7dd024bd3c06911708
Quoc Anh <quocanh@Quocs-MBP.home>
2022-04-26 10:14:53 +0200

    first commit

---
+++ src/main/java/de/htwberlin/webtech/healthyrecipes/HealthyrecipesApplication.java
@@ -0,0 +1,13 @@
+package de.htwberlin.webtech.healthyrecipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthyrecipesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthyrecipesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/htwberlin/webtech/healthyrecipes/HealthyrecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htwberlin.webtech.healthyrecipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthyrecipesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
