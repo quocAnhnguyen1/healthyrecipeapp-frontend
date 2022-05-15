06232b5fdc87de2da6b62f5b88424f6400f5da08
quocAnhnguyen1 <Quoc.Nguyen@Student.htw-berlin.de>
2022-05-15 21:59:19 +0200

    no new changes

--- src/main/java/de/htwberlin/webtech/healthyrecipes/web/RecipeRestController.java
+++ src/main/java/de/htwberlin/webtech/healthyrecipes/web/RecipeRestController.java
@@ -1,2 +1,28 @@
-package de.htwberlin.webtech.healthyrecipes.web;public class RecipeRestController {
+package de.htwberlin.webtech.healthyrecipes.web;
+
+import de.htwberlin.webtech.healthyrecipes.web.api.Recipe;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@RestController
+public class RecipeRestController {
+
+    private List<Recipe> recipes;
+    public RecipeRestController(){
+        recipes = new ArrayList<>();
+        recipes.add(new Recipe("Salmon Pasta", "Easy pasta recipe with salmon and asparagus", 30, "easy", 0));
+        recipes.add(new Recipe("Pho", "Traditional pho recipe", 60 , "easy", 0));
+    }
+
+    @GetMapping(path = "/api/v1/recipes")
+    public ResponseEntity<List<Recipe>> fetchRecipes(){
+        return ResponseEntity.ok(recipes);
+    }
+
 }
--- src/main/java/de/htwberlin/webtech/healthyrecipes/web/api/Recipe.java
+++ src/main/java/de/htwberlin/webtech/healthyrecipes/web/api/Recipe.java
@@ -1,2 +1,57 @@
-package de.htwberlin.webtech.healthyrecipes.web.api;public class Recipe {
+package de.htwberlin.webtech.healthyrecipes.web.api;
+
+public class Recipe {
+    private String recipeName;
+    private String recipeDescription;
+    private long time;
+    private String difficulty;
+    private int likes;
+
+    public Recipe(String recipeName, String recipeDescription, long time, String difficulty, int likes){
+        this.recipeName = recipeName;
+        this.recipeDescription = recipeDescription;
+        this.time = time;
+        this.difficulty = difficulty;
+        this.likes = likes;
+    }
+
+    public String getRecipeName() {
+        return recipeName;
+    }
+
+    public void setRecipeName(String recipeName) {
+        this.recipeName = recipeName;
+    }
+
+    public String getRecipeDescription() {
+        return recipeDescription;
+    }
+
+    public void setRecipeDescription(String recipeDescription) {
+        this.recipeDescription = recipeDescription;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+
+    public String getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(String difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public void setLikes(int likes) {
+        this.likes = likes;
+    }
 }
