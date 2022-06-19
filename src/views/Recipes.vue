9fd6d73e80fef044ade849f5132a7d1567c4bdab
quocAnhnguyen1 <Quoc.Nguyen@Student.htw-berlin.de>
2022-06-19 18:17:01 +0200

    add buttons to recipes site

--- src/views/Recipes.vue
+++ src/views/Recipes.vue
@@ -1,11 +1,38 @@
 <template>
   <h1>Welcome to recipes</h1>
+  <div class="d-grid gap-2 col-6 mx-auto">
+    <div class="col" v-for="recipe in recipes" :key="recipe.recipeID">
+      <button class="btn btn-primary" type="button">{{ recipe.recipeName }}</button>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
-  name: 'Recipes'
+  name: 'Recipes',
+  data () {
+    return {
+      recipes: [
+        {
+          recipeID: 1,
+          recipeName: 'Chickenburger with avocado sauce',
+          recipeDescription: 'Simple and healthy Chickenburger',
+          time: 30,
+          difficulty: 'easy',
+          likes: 2
+        },
+        {
+          recipeID: 2,
+          recipeName: 'Porridge with fruits',
+          recipeDescription: 'Simple and healthy porridge',
+          time: 15,
+          difficulty: 'easy',
+          likes: 1
+        }
+      ]
+    }
+  }
 }
 </script>
 
