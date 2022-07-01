19315a7124274a9062353a118ae2ce3f5e66ea93
quocAnhnguyen1 <Quoc.Nguyen@Student.htw-berlin.de>
2022-07-01 12:27:20 +0200

    Milestone 5 video 4

--- src/components/RecipesCreateForm.vue
+++ src/components/RecipesCreateForm.vue
@@ -8,22 +8,34 @@
       <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
     </div>
     <div class="offcanvas-body">
-      <form class="text-start">
+      <form class="text-start needs-validation" novalidate>
         <div class="mb-3">
           <label for="recipe-name" class="form-label">Recipe name</label>
-          <input type="text" class="form-control" id="recipe-name" v-model="recipeName">
+          <input type="text" class="form-control" id="recipe-name" v-model="recipeName" required>
+          <div class="invalid-feedback">
+            Please provide a name for your recipe.
+          </div>
         </div>
         <div class="mb-3">
           <label for="recipe-description" class="form-label">Description</label>
-          <input type="text" class="form-control" id="recipe-description" v-model="recipeDescription">
+          <input type="text" class="form-control" id="recipe-description" v-model="recipeDescription" required>
+          <div class="invalid-feedback">
+            Please provide a description for your recipe with clear steps on how to make it.
+          </div>
         </div>
         <div class="mb-3">
           <label for="recipe-difficulty" class="form-label">Difficulty</label>
-          <input type="text" class="form-control" id="recipe-difficulty" v-model="difficulty">
+          <input type="text" class="form-control" id="recipe-difficulty" v-model="difficulty" required>
+          <div class="invalid-feedback">
+            Please provide a difficulty for your recipe. (easy, medium, hard, professional)
+          </div>
         </div>
         <div class="mb-3">
           <label for="recipe-time" class="form-label">Time</label>
-          <input type="text" class="form-control" id="recipe-time" v-model="time">
+          <input type="text" class="form-control" id="recipe-time" v-model="time" required>
+          <div class="invalid-feedback">
+            Please provide an estimated cooking time for your recipe.
+          </div>
         </div>
         <div class="mt-5">
           <button class="btn btn-primary me-3" type="submit" @click="createRecipe">Create</button>
@@ -47,28 +59,51 @@ export default {
   },
   methods: {
     createRecipe () {
-      const endpoint = process.env.VUE_APP_BACKEND_BASE_URL + '/api/v1/recipes'
+      const valid = this.validate()
+      if (valid) {
+        const endpoint = process.env.VUE_APP_BACKEND_BASE_URL + '/api/v1/recipes'
 
-      const headers = new Headers()
-      headers.append('Content-Type', 'application/json')
+        const headers = new Headers()
+        headers.append('Content-Type', 'application/json')
 
-      const payload = JSON.stringify({
-        recipeName: this.recipeName,
-        recipeDescription: this.recipeDescription,
-        time: this.time,
-        difficulty: this.difficulty,
-        likes: 0
+        const payload = JSON.stringify({
+          recipeName: this.recipeName,
+          recipeDescription: this.recipeDescription,
+          time: this.time,
+          difficulty: this.difficulty,
+          likes: 0
+        })
+
+        const requestOptions = {
+          method: 'POST',
+          headers: headers,
+          body: payload,
+          redirect: 'follow'
+        }
+
+        fetch(endpoint, requestOptions)
+          .catch(error => console.log('error', error))
+      }
+    },
+    validate () {
+      let valid = true
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      var forms = document.querySelectorAll('.needs-validation')
+
+      // Loop over them and prevent submission
+      Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
+          if (!form.checkValidity()) {
+            valid = false
+            event.preventDefault()
+            event.stopPropagation()
+          }
+
+          form.classList.add('was-validated')
+        }, false)
       })
 
-      const requestOptions = {
-        method: 'POST',
-        headers: headers,
-        body: payload,
-        redirect: 'follow'
-      }
-
-      fetch(endpoint, requestOptions)
-        .catch(error => console.log('error', error))
+      return valid
     }
   }
 }
